1a3c7cd540f7fb29ae5823b8992597b1f1e9e7b9
Austin Schrader <austin.jschrader@gmail.com>
2019-02-19 22:39:25 -0800

    Divided the logic of the car object based program into 2 files

---
+++ Models/Car.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Dealership {
+
+  class Car
+  {
+    public string MakeModel;
+    public int Price;
+    public int Miles;
+
+    public Car(string makeModel, int price, int miles)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+    }
+
+    public bool WorthBuying(int maxPrice)
+    {
+      return (Price < maxPrice);
+    }
+  }
+
+}
